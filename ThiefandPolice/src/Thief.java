27ece223f3699b1f2c6edda2ac97d90469feea7e
Iman Tabrizian <Tabrizian@outlook.com>
2015-04-10 23:28:15 +0430

    Minor bugs fixed!

--- ThiefandPolice/src/Thief.java
+++ ThiefandPolice/src/Thief.java
@@ -1,31 +1,33 @@
+public class Thief extends Actor {
 
-public class Thief extends Actor{
-	
-	public Thief(Map map){
-		super('D',map);
+	public Thief(Map map) {
+		super('D', map);
 	}
-	
-	public Direction getDirection(Map map){
+
+	public Direction getDirection(Map map) {
 		int min = 10;
-		Direction direction = null; //direction in which polices are minimum,
+		Direction direction = null; // direction in which polices are minimum,
 		Position[] neighbs = Neighbor.getNeighb(pos, map);
 		for (int i = 0; i < neighbs.length; i++) {
 			for (Position position : neighbs) {
 				int numberOfPolices = 0;
-				Position[] neighbsOfNeighbs = Neighbor.getNeighb(position, map);
-				for (int j = 0; j < neighbsOfNeighbs.length; j++) {
-					if(neighbsOfNeighbs[j] != null){
-						if(map.getPos(neighbsOfNeighbs[j])=='P')
-							numberOfPolices++;
+				if (position != null) {
+					Position[] neighbsOfNeighbs = Neighbor.getNeighb(position,
+							map);
+					for (int j = 0; j < neighbsOfNeighbs.length; j++) {
+						if (neighbsOfNeighbs[j] != null) {
+							if (map.getPos(neighbsOfNeighbs[j]) == 'P')
+								numberOfPolices++;
+						}
 					}
 				}
-				if(numberOfPolices < min){
+				if (numberOfPolices < min) {
 					min = numberOfPolices;
 					direction = Direction.values()[i];
 				}
 			}
 		}
 		return direction;
-		
+
 	}
 }
